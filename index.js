bfdabc61a1fe3a454d7737fe832d6b8a84db0c36
+ <+>
2023-07-05 17:57:27 +0300

    added event listener to 2nd recipe name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const allRecipes = document.getElementById('recipes')
 const corbaRecipe = document.getElementById('corba')
 const corbaIng = document.getElementById('corbaingredients')
 const burekRecipe = document.getElementById('burek')
-const burekIng = document.getElementById('burekingridients')
+const burekIng = document.getElementById('burekingredients')
 
 fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s`)
     .then(response => response.json())
@@ -10,6 +10,7 @@ fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s`)
         console.log(data.meals)
       //  data.meals.forEach(dataitem => {
         corbaTurkish(data)
+        burekCroatian (data)
         
       //  })
     })
@@ -64,8 +65,48 @@ function corbaRecipeInstructions(data) {
 
 }
 
-function BurekCroatian (data) {
+function burekCroatian (data) {
     const burekImg = document.createElement('img')
     burekImg.src = data.meals[1].strMealThumb
+    burekRecipe.appendChild(burekImg)
 
+    const burekCountry = document.createElement('p')
+    burekCountry.innerText = 'Country : Croatia ' + '- ' + ' This is a ' + data.meals[1].strArea + ' dish'
+    burekRecipe.appendChild(burekCountry)
+
+    const burekName = document.createElement ('p')
+    burekName.innerText = data.meals[1].strMeal
+    burekRecipe.appendChild(burekName)
+
+    burekName.addEventListener('click', () => {
+        burekRecipeInstructions(data)
+    })
+}
+
+function burekRecipeInstructions(data) {
+    burekIng.innerHTML = ""
+
+    const burekFilo = document.createElement('li')
+    burekFilo.innerText = data.meals[1].strIngredient1
+    burekIng.appendChild(burekFilo)
+
+    const corbaOnions = document.createElement('li')
+    corbaOnions.innerText = data.meals[0].strIngredient2
+    corbaIng.appendChild(corbaOnions)
+
+    const corbaCarrots = document.createElement('li')
+    corbaCarrots.innerText = data.meals[0].strIngredient3
+    corbaIng.appendChild(corbaCarrots)
+
+    const corbaTomato = document.createElement('li')
+    corbaTomato.innerText = data.meals[0].strIngredient4
+    corbaIng.appendChild(corbaTomato)
+
+    const corbaCumin = document.createElement('li')
+    corbaCumin.innerText = data.meals[0].strIngredient5
+    corbaIng.appendChild(corbaCumin)
+
+    const corbaInstructions = document.createElement('p')
+    corbaInstructions.innerText = data.meals[0].strInstructions
+    corbaIng.appendChild(corbaInstructions)
 }
