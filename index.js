0170546bd4ad4d819986e9c2d77464f1d49f1b00
+ <+>
2023-07-05 12:07:49 +0300

    First draft of html structure in index.html

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-
-
+const allRecipes = document.getElementById('recipes')
 
 fetch ("https://www.themealdb.com/api/json/v1/1/search.php?s")
 .then(response => response.json())
 .then(data => {console.log(data)})
 
+function totalRecipes(){
+    
+}
+totalRecipes()
