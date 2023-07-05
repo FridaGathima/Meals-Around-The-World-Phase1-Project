2b629670f42fdb43ed318cceed1fc87f60765143
+ <+>
2023-07-05 15:27:29 +0300

    Successful fetch from API

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const allRecipes = document.getElementById('recipes')
 
-fetch ("https://www.themealdb.com/api/json/v1/1/search.php?s")
-.then(response => response.json())
-.then(data => {console.log(data)})
 
-function totalRecipes(){
-    
+function totalRecipes() {
+fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s`)
+    .then(response => response.json())
+    .then((data) => {
+        console.log(data.meals)
+        data.meals.forEach(dataitem => {
+
+        // const recipeIntroAll = document.createElement('p')
+        // recipeIntroAll.innerText = ''
+        // allRecipes.appendChild(recipeIntroAll)
+
+        const recipeName = document.createElement('img')
+        recipeName.src = dataitem.strMealThumb
+        allRecipes.appendChild(recipeName)
+        })
+    })
+
 }
 totalRecipes()
