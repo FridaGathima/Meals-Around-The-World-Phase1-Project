aeec95745689969fb854ee43dac91a3dc7b34de4
+ <+>
2023-07-05 16:26:09 +0300

    Successful display 1st recipe

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const allRecipes = document.getElementById('recipes')
+const corbaRecipe = document.getElementById('corba')
 
 
-function totalRecipes() {
 fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s`)
     .then(response => response.json())
     .then((data) => {
         console.log(data.meals)
-        data.meals.forEach(dataitem => {
-
-        // const recipeIntroAll = document.createElement('p')
-        // recipeIntroAll.innerText = ''
-        // allRecipes.appendChild(recipeIntroAll)
-
-        const recipeName = document.createElement('img')
-        recipeName.src = dataitem.strMealThumb
-        allRecipes.appendChild(recipeName)
-        })
+       // data.meals.forEach(dataitem => {
+        corbaTurkish(data)
+        
+      //  })
     })
 
+function corbaTurkish(data) {
+
+    const corbaImg = document.createElement('img')
+    corbaImg.src = data.meals[0].strMealThumb
+    corbaRecipe.appendChild(corbaImg)
+
+    const corbaCountry = document.createElement('p')
+    corbaCountry.innerText = data.meals[0].strArea
+    corbaRecipe.append(corbaCountry)
+
+    const corbaName = document.createElement('p')
+    corbaName.innerText = data.meals[0].strMeal
+    corbaRecipe.append(corbaName)
+
 }
-totalRecipes()
+
+function corbaRecipeInstructions(data)
