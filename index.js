3b1378df0a55793691e0e64b4d9a519831e17504
+ <+>
2023-07-05 18:06:15 +0300

    Done recipe 2

--- index.js
+++ index.js
@@ -90,23 +90,23 @@ function burekRecipeInstructions(data) {
     burekFilo.innerText = data.meals[1].strIngredient1
     burekIng.appendChild(burekFilo)
 
-    const corbaOnions = document.createElement('li')
-    corbaOnions.innerText = data.meals[0].strIngredient2
-    corbaIng.appendChild(corbaOnions)
+    const burekBeef = document.createElement('li')
+    burekBeef.innerText = data.meals[1].strIngredient2
+    burekIng.appendChild(burekBeef)
 
-    const corbaCarrots = document.createElement('li')
-    corbaCarrots.innerText = data.meals[0].strIngredient3
-    corbaIng.appendChild(corbaCarrots)
+    const burekOnion = document.createElement('li')
+    burekOnion.innerText = data.meals[1].strIngredient3
+    burekIng.appendChild(burekOnion)
 
-    const corbaTomato = document.createElement('li')
-    corbaTomato.innerText = data.meals[0].strIngredient4
-    corbaIng.appendChild(corbaTomato)
+    const burekOil = document.createElement('li')
+    burekOil.innerText = data.meals[1].strIngredient4
+    burekIng.appendChild(burekOil)
 
-    const corbaCumin = document.createElement('li')
-    corbaCumin.innerText = data.meals[0].strIngredient5
-    corbaIng.appendChild(corbaCumin)
+    const burekSalt = document.createElement('li')
+    burekSalt.innerText = data.meals[1].strIngredient5
+    burekIng.appendChild(burekSalt)
 
-    const corbaInstructions = document.createElement('p')
-    corbaInstructions.innerText = data.meals[0].strInstructions
-    corbaIng.appendChild(corbaInstructions)
+    const burekInstructions = document.createElement('p')
+    burekInstructions.innerText = data.meals[1].strInstructions
+    burekIng.appendChild(burekInstructions)
 }
